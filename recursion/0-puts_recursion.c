091dbf404ff8ba157e4972c5aa0405b9d88e3047
Phinatech <c.ugbo@alustudent.com>
2025-10-19 19:09:14 +0200

    fix: recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,23 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-int main(void)
-{
-    _puts_recursion("Hello, recursion!");
-    return (0);
-}
 /**
  *_puts_recursion - prints a string
  *@s: pointer block of memory to fill
