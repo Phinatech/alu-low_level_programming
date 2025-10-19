44f3f96204fe0bc8562e275d050d1e0f2d0b8c65
Phinatech <c.ugbo@alustudent.com>
2025-10-19 19:48:37 +0200

    add files

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -1,4 +1,44 @@
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: The argument count.
+ * @av: The argument vector.
+ * Return: A pointer to a new string, or NULL if it fails.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len += strlen(av[i]);
+		len++; /* for the newline character */
+	}
+
+	str = malloc(sizeof(char) * (len + 1)); /* +1 for null terminator */
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
+}
 
 /**
  * free_grid - a function that frees a 2 dimensional grid.
