a718fe034bc62ca5e1fe90120919c763bcc82aa4
Phinatech <c.ugbo@alustudent.com>
2025-10-24 16:53:57 +0200

    pushing

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,4 +14,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* DOG_H */
+
