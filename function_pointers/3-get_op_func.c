343b43d06ea75bbee1e7cfac56242010279020e7
Phinatech <c.ugbo@alustudent.com>
2025-11-06 11:52:41 +0200

    update

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,7 +5,7 @@
  * get_op_func - selects the correct function to perform the operation
  * @s: operator passed as argument
  *
- * Return: pointer to the function corresponding to the operator given as a parameter
+ * Return: pointer to the function corresponding to the operator given
  */
 int (*get_op_func(char *s))(int, int)
 {
